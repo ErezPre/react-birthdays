fcb9d5e74b0f97a6134484db63d91bd632650994
Erez Prepelitzki <prepelitzki@gmail.com>
2022-04-21 08:11:36 +0300

    1

--- src/App.js
+++ src/App.js
@@ -6,16 +6,41 @@ import data from "./data";
 
 function App() {
   const [people, setPeople] = useState(data);
+  const date1 = new Date();
+  console.log(new Date().getTime());
+  console.log(
+    `${date1.getDate()}/${date1.getMonth() + 1}/${date1.getFullYear()}`
+  );
+  const name = useRef(null);
+  const date = useRef(null);
   return (
     <>
       <h1>birthdays list:</h1>
       <form
         onSubmit={(e) => {
           e.preventDefault();
+          console.log(name.current.value);
+          console.log(date.current.value);
           console.log("submit");
+
+          const dateNewFormatArray = date.current.value.split("-");
+          const dateNewFormat = `${parseInt(dateNewFormatArray[2])}/${parseInt(
+            dateNewFormatArray[1]
+          )}/${parseInt(dateNewFormatArray[0])}`;
+
+          const birthday = {
+            id: new Date().getTime(),
+            name: name.current.value,
+            birthday: dateNewFormat,
+          };
+          console.log(birthday);
+          setPeople([...people, birthday]);
         }}
       >
-        <input type="text" />
+        <label htmlFor="name"></label>
+        <input type="text" name="name" ref={name} />
+        <label htmlFor="date"></label>
+        <input type="date" name="date" ref={date} />
         <button type="submit">add birthday</button>
       </form>
       <br />
--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export default [
-  { id: 1, name: "John", birthday: "1/1/70" },
-  { id: 2, name: "George", birthday: "2/3/71" },
-  { id: 3, name: "Paul", birthday: "4/6/72" },
-  { id: 4, name: "Richard", birthday: "1/8/75" },
+  { id: 1, name: "John", birthday: "1/1/1970" },
+  { id: 2, name: "George", birthday: "2/3/1971" },
+  { id: 3, name: "Paul", birthday: "4/6/1972" },
+  { id: 4, name: "Richard", birthday: "1/8/1975" },
 ];
